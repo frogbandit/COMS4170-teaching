94019e50f6d42aa034a6effa26471a980282fb0b
James Xue <jamesxue@Jamess-MacBook-Pro.local>
2017-09-28 01:53:13 -0400

    Add CORS

---
+++ census-cors/script_error.js
@@ -0,0 +1,24 @@
+// example API request: https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4600+Silver+Hill+Rd%2C+Suitland%2C+MD+20746&benchmark=9&format=json
+
+// what's wrong with this?
+function validateAddress(query){
+ $.ajax({
+		url: "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?format=json",
+		dataType: "json",
+    data: {
+			 address: query,
+			 benchmark: 9,
+    },
+    success: function(response){
+      console.log(response);
+     $("#validated-address").text(response.result.addressMatches[0].matchedAddress);
+		}
+ });
+}
+
+$(document).ready(function() {
+  document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault(); //prevent a submit button from submitting a form.
+    validateAddress(document.getElementById('address').value);
+}, false);
+});
